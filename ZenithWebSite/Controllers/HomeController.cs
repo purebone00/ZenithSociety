30ba8117d0a2f355df1df277cad24752be00aae3
Albert <purebone00@gmail.com>
2017-02-10 17:26:47 -0800

    added the home page

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using ZenithDataLib.Models;
+using ZenithSociety.Utility;
 
 namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+        
         public ActionResult Index()
         {
-            return View();
+            var firstdayOfThisWeek = DateTime.Now.FirstDayOfWeek();
+            var lastdayOfThisWeek = DateTime.Now.LastDayOfWeek();
+
+            var ActivityEventsList = from e in db.Events
+                                     join a in db.Activities on e.ActivityId equals a.ActivityId
+                                     where (e.StartDateTime >= firstdayOfThisWeek)
+                                     && (e.StartDateTime < lastdayOfThisWeek)
+                                     && (e.IsActive == true)
+                                     orderby (e.CreatedTime)
+                                     select new EventActivityModel { Event = e, Activity = a };
+    
+            return View(ActivityEventsList);
         }
 
         public ActionResult About()
@@ -25,6 +42,12 @@ namespace ZenithWebSite.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public class EventActivityModel
+        {
+            public Event Event { get; set; }
+            public Activity Activity { get; set; }
         }
     }
 }
